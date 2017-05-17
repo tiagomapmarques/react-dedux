1eb936f35faf454812b30920cfba39b371f9172a
Tiago Marques <tiago.map.marques@gmail.com>
2017-05-17 14:37:32 +0100

    Minor renaming

--- src/connect.ts
+++ src/connect.ts
@@ -1,4 +1,4 @@
-import { connect as reduxConnect } from 'react-redux';
+import * as Redux from 'react-redux';
 
 import { AnyConfiguration, ActionGroupList, Connector } from './types';
 import { getConfig } from './config';
@@ -8,7 +8,7 @@ import { stateChangersSelector } from './state-changers';
 export const connect = (actions: ActionGroupList, config?: AnyConfiguration): Connector =>
   (...args: string[]) => {
     const configuration = getConfig(config);
-    return reduxConnect(
+    return Redux.connect(
       stateChangersSelector(...args),
       actionsSelector(
         Object.keys(actions).reduce((accumulator: ActionGroupList, key) => ({

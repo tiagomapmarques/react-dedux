bbe250499df8a8fee834711777aefd0542e63180
Tiago Marques <tmarques@ventureoak.com>
2017-05-17 12:22:33 +0100

    Removed configuration from being mandatory
    Also cleaned some code and merged state changers and their default values in one variable for a better API.

--- src/connect.ts
+++ src/connect.ts
@@ -1,16 +1,21 @@
-import { Configuration, ActionGroupList, Connector } from './types';
 import { connect as reduxConnect } from 'react-redux';
+
+import { AnyConfiguration, ActionGroupList, Connector } from './types';
+import { getConfig } from './config';
 import { actionsSelector } from './actions';
 import { stateChangersSelector } from './state-changers';
 
-export const connect = (actions: ActionGroupList, config: Configuration): Connector => (...args: string[]) =>
-  reduxConnect(
-    stateChangersSelector(...args),
-    actionsSelector(
-      Object.keys(actions).reduce((accumulator: ActionGroupList, key) => ({
-        ...accumulator,
-        [config.getActionsName(key)]: actions[key],
-      }), {}),
-      config
-    )(...args)
-  );
+export const connect = (actions: ActionGroupList, config?: AnyConfiguration): Connector =>
+  (...args: string[]) => {
+    const configuration = getConfig(config);
+    return reduxConnect(
+      stateChangersSelector(...args),
+      actionsSelector(
+        Object.keys(actions).reduce((accumulator: ActionGroupList, key) => ({
+          ...accumulator,
+          [configuration.getActionsName(key)]: actions[key],
+        }), {}),
+        configuration
+      )(...args)
+    );
+  }
--- src/state-changers.ts
+++ src/state-changers.ts
@@ -1,6 +1,6 @@
 import { StateChangerGroupList } from './types';
 
-export const stateChangersSelector = (...args: string[]) => (reducers: StateChangerGroupList): StateChangerGroupList =>
-  Object.keys(reducers)
+export const stateChangersSelector = (...args: string[]) => (stateChangers: StateChangerGroupList): StateChangerGroupList =>
+  Object.keys(stateChangers)
     .filter(key => args.indexOf(key) >= 0)
-    .reduce((accumulator, key) => ({ ...accumulator, [key]: reducers[key] }), {});
+    .reduce((accumulator, key) => ({ ...accumulator, [key]: stateChangers[key] }), {});

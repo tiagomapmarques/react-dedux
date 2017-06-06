6da6df637834afbf0cdbb37ee0d2b691dcd701f6
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2017-06-06 10:20:11 +0100

    Feature: Prevent configuration errors (#13)
    * Added prevention of configuration errors
    
    * Updated ComponentDecorator usage
    
    * Ran build command
    
    * Updated dependencies and bumped version

--- src/combine-reducers.ts
+++ src/combine-reducers.ts
@@ -11,8 +11,7 @@ import {
 
 const reducer = <S>(typeName: string, defaultValue: any, stateChangers: StateChangerGroup<S>, config: Configuration): StateChangerReduced<S> =>
   (prevState: S, action: ActionObject): S => {
-    const splitter = config.SPLITTER;
-    const actionType = action.type.split(splitter);
+    const actionType = action.type.split(config.getSplitter());
     if (actionType.length === 2 && actionType[0].toLowerCase() === typeName.toLowerCase()) {
       return stateChangers[actionType[1]](prevState, action);
     }
